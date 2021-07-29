6d987215d3e4cc8d4fba142ce6f01e7dc3a5c1a8
Shellejd <rochelle.dishmon@gmail.com>
2021-07-29 14:00:49 -0400

    first commit

---
+++ src/main/java/wcci/studyproteam/studypro/StudyproApplication.java
@@ -0,0 +1,13 @@
+package wcci.studyproteam.studypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wcci/studyproteam/studypro/StudyproApplicationTests.java
@@ -0,0 +1,13 @@
+package wcci.studyproteam.studypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
