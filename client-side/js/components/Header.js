4235352d37fdbd31031a33e9b129795c1394559f
Patel10 <parth1029@yahoo.com>
2021-08-10 11:16:41 -0400

    layout - header

---
+++ client-side/js/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header(){
+    return `
+    <ul class='nav_bar'>
+    
+    <li class='nav_home'>HOME</li>
+    <li class='nav_hashtags'>HASHTAGS</li>
+    <li class='nav_search'>SEARCH BY</li>
+    <li class='nav_sign'>SIGN UP</li>
+    <li class='nav_about'>ABOUT US</li>
+    <li class='nav_contact'>CONTACT US</li>
+
+    </ul>
+
+    `
+}
