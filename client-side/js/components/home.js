3833c1dec3ce0af718129f0e9da836bda81e015b
Shellejd <rochelle.dishmon@gmail.com>
2021-08-25 20:12:29 -0400

    changes to css

--- client-side/js/components/home.js
+++ client-side/js/components/home.js
@@ -1,26 +1,33 @@
 import puzzle2 from "../../images/puzzle.png";
 import puzzle from "../../images/contribution.png";
-export default function home()
-{
+import lovetolearn from "../../images/lovetolearn.jpg";
+import booksapple from "../../images/booksapple.jpg";
+import kidsjumping from "../../images/kidsjumping.jpg";
+
+
+export default function home() {
     return `
     <div>
     <h1> WELCOME TO STUDYPRO! </h1>
    
-    <p>Rated the best!</p>
+    <h2>Rated the best!</h2>
 
-<p> Our learning platform is developed by the best developers with backgrounds in education and management. StudyPRO provides an engaging learning experience in varies topics.
-</p>
+<h3> Our learning platform is developed by the best developers with backgrounds in education and management. StudyPRO provides an engaging learning experience in varies topics.
+</h3>
 </div>
 
 <div class="testing">
-<h1 class="testing1">Become your most unstoppable self</h1>
+<h2 class="testing1">Become your most unstoppable self</h2>
 <div class=testing2">
-<p>Master any subject, one success at a time.</p>
+<h3>Master any subject, one success at a time.</h3>
 
 
 <div id="slideshow">
  <img class="mySlides" src="${puzzle2}" style="width:50%">
  <img class="mySlides" src="${puzzle}" style="width:50%">
+ <img class="mySlides" src="${lovetolearn}" style="width:50%">
+ <img class="mySlides" src="${booksapple}" style="width:50%">
+ <img class="mySlides" src="${kidsjumping}" style="width:50%">
 </div>
 
     `;
